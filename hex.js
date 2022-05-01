9d7687c4d6cd5685b9cf0be26cc27d97fe25e536
Meehir Mani Tripathy <meehirmanitrips@gmail.com>
2022-05-01 11:29:49 +0530

    hex page finished

--- hex.js
+++ hex.js
@@ -1,1 +1,11 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector(`.btn`);
+const color = document.querySelector(`.color`);
+btn.addEventListener(`click`, () => {
+  const letters = "0123456789ABCDEF";
+  let randomColor = "#";
+  for (let i = 0; i < 6; ++i) {
+    randomColor += letters[Math.floor(Math.random() * letters.length)];
+  }
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+});
