b6aa4c83e4804f6f2ff327d5127933935fd8f3e9
Meehir Mani Tripathy <meehirmanitrips@gmail.com>
2022-05-01 10:54:25 +0530

    simple page finished

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [`green`, `red`, `rgba(133, 122, 200)`, `#f15025`];
+const btn = document.querySelector(`.btn`);
+const color = document.querySelector(`.color`);
+btn.addEventListener(`click`, () => {
+  //Get Random Number between 0 and 3
+  const randomNumber = Math.floor(Math.random() * colors.length);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.innerHTML = colors[randomNumber];
+});
